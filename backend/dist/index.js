75d1aa386e4ab61bd2ec246762b1ac098fd1fae8
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-08-30 22:20:39 +0200

    feat

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
@@ -12,8 +11,7 @@ app.use((0, cors_1.default)({
     credentials: true,
 }));
 app.use(body_parser_1.default.json());
-const server = http_1.default.createServer(app);
-server.listen(8080, () => {
+app.listen(8080, () => {
     console.log("Server runnig on http://localhost:8080/");
 });
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import http from "http";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -14,8 +13,6 @@ app.use(
 
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-
-server.listen(8080, () => {
+app.listen(8080, () => {
   console.log("Server runnig on http://localhost:8080/");
 });
