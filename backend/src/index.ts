9143b6573514fdd280edff0703f33a5e0429de1b
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-08-31 12:36:33 +0200

    feat:documentation how to getting started.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
