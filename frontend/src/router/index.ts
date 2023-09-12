d83572b2eb081dda1e6157a08a9f003bc79e3161
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-12 16:04:28 +0200

    add

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Login from "@/components/login.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
