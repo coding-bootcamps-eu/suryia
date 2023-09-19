762f0cf7d75b92ffd974cb41ed9df038e73e64f1
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-19 19:25:55 +0200

    add

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,3 +6,4 @@ export const PORT: number = parseInt(process.env.PORT || "8080", 10);
 export const MONGODB_URI: string =
   process.env.MONGODB_URI || "mongodb://localhost:27017/suriya";
 export const API_VERSION: string = process.env.API_VERSION || "1.5.5";
+export const PASSPORT_SECRET: string = "nodeauthsecret";
