30db5697bdcb06276db298966f0e80e8dcc567dc
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-20 15:51:14 +0200

    fix router

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,14 +3,23 @@ import passport from "passport";
 
 const router = express.Router();
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err: string, user: string) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Anmeldung fehlgeschlagen" });
+    }
+    // Anmeldung erfolgreich
+    req.login(user, (loginErr) => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+      return res.json({ message: "Anmeldung erfolgreich" });
+    });
+  })(req, res, next);
+});
 
 router.get("/logout", (req, res) => {
   req.logout((err) => {
