3afb06edcff36a7c1a7ba232a18e638e71f67a39
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-21 14:07:15 +0200

    add bcrypt

--- backend/src/models/Users.ts
+++ backend/src/models/Users.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from "mongoose";
 import passportLocalMongoose from "passport-local-mongoose";
+import bcrypt from "bcrypt";
 export interface User extends Document {
   id: string;
   email: string;
@@ -12,5 +13,20 @@ const UserSchema: Schema<User> = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
 UserSchema.plugin(passportLocalMongoose);
+
 export const UserModel = mongoose.model("User", UserSchema);
