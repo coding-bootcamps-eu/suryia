8e664dd751011e0f862f0821e6ee6cd5c0786ea5
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-21 15:51:01 +0200

    fix

--- backend/src/controller/accountController.ts
+++ backend/src/controller/accountController.ts
@@ -4,39 +4,38 @@ import { UserModel } from "../models/Users";
 import jwt from "jwt-simple";
 import { config } from "../config";
 
-export default{
-   login : async (req: Request, res: Response) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
+export default {
+  login: async (req: Request, res: Response) => {
+    try {
+      const user = await UserModel.findOne({ email: req.body.email });
 
-    if (!user) {
-      return res.status(404).json({ error: "Benutzer nicht gefunden" });
+      if (!user) {
+        return res.status(404).json({ error: "Invalid user" });
+      }
+
+      const payload = {
+        id: user.id,
+        expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      };
+
+      const token = jwt.encode(payload, config.jwtSecret);
+
+      res.json({ token: token });
+    } catch (err) {
+      console.error("Fehler", err);
+      res.status(500).json({ error: "Invalid process" });
     }
-
-    const payload = {
-      id: user.id,
-      expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    };
-
-    const token = jwt.encode(payload, config.jwtSecret);
-
-    res.json({ token: token });
-  } catch (err) {
-    console.error("Fehler", err);
-    res.status(500).json({ error: "Ein Fehler ist aufgetreten" });
-  }
+  },
+  register: async (req: Request, res: Response) => {
+    try {
+      const newUser = new UserModel({
+        email: req.body.email,
+      });
+      await UserModel.register(newUser, req.body.password);
+      res.json({ message: "Successful registration!" });
+    } catch (err) {
+      console.error("Invalid registration", err);
+      res.status(500).json({ error: "Invalid registration" });
+    }
+  },
 };
-register : async (req: Request, res: Response) => {
-  try {
-    const newUser = new UserModel({
-      email: req.body.email,
-    });
-    await UserModel.register(newUser, req.body.password);
-    res.json({ message: "Successful registration!" });
-  } catch (err) {
-    console.error("Invalid registration", err);
-    res.status(500).json({ error: "Invalid registration" });
-  }
-}
-
-}
