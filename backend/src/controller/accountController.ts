772cfc6a16dafe48904bd614aee2786585a2597e
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-09-22 16:48:56 +0200

    add /status

--- backend/src/controller/accountController.ts
+++ backend/src/controller/accountController.ts
@@ -3,6 +3,9 @@ import passportLocalMongoose from "passport-local-mongoose";
 import { UserModel } from "../models/Users";
 import jwt from "jwt-simple";
 import { config } from "../config";
+import mongoose from "mongoose";
+import { Status } from "../models/Status";
+import { API_VERSION } from "../config";
 
 export default {
   login: async (req: Request, res: Response) => {
@@ -40,6 +43,30 @@ export default {
     } catch (err) {
       console.error("Invalid registration", err);
       res.status(500).json({ error: "Invalid registration" });
+    }
+  },
+  getStatus: async (req: Request, res: Response) => {
+    try {
+      const secretToken = req.query.secret_token as string;
+      if (!secretToken) {
+        return res.status(400).json({ error: "Unauthorized" });
+      }
+      const dbStatus = mongoose.connection.readyState === 1;
+      const status = await Status.findOne({});
+
+      console.log("Status saved successfully");
+      res.json({
+        message: "Status retrieved successfully",
+        status: {
+          db: dbStatus,
+        },
+        api_version: API_VERSION,
+        user: req.user,
+        token: secretToken,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
     }
   },
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,29 +17,6 @@ app.use(express.json());
 //Connection to mongodb
 connectToDB();
 
-//Return the MongoDB connection status and store the API version in the database:
-app.get("/status", async (req: Request, res: Response) => {
-  try {
-    const dbStatus = mongoose.connection.readyState === 1;
-    const status = await Status.findOne({});
-
-    if (!status) {
-      console.log("Status not found!");
-      res.status(404).json({ error: "Status not found!" });
-    }
-    console.log("Status saved successfully");
-    res.json({
-      status: {
-        db: dbStatus,
-      },
-      api_version: API_VERSION,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 app.use(
   cors({
     //origin: "https://google.com",
@@ -65,18 +42,14 @@ app.use(
 const passportMiddleware = initializePassport();
 app.use(passportMiddleware.initialize());
 
-passport.use(
-  new LocalStrategy(
-    //{ usernameField: "email", passwordField: "password" },
-    UserModel.authenticate()
-  )
-);
+passport.use(new LocalStrategy(UserModel.authenticate()));
 passport.serializeUser(UserModel.serializeUser());
 app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.send("Introduction JWT Auth");
 });
+app.get("/status", accountController.getStatus);
 app.post("/login", passport.authenticate("local"), accountController.login);
 app.post("/register", accountController.register);
 
