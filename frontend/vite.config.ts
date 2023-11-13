63ab24f6d9a8dc04bd497b4e42213535722b85fb
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-11-13 13:33:10 +0100

    config quasar

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/styles/quasar-variables.sass'
+    })
   ],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@import 'quasar/src/css/index.sass';`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
