7edce02d33b603d201227ad459637ecc63825e07
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-11-20 18:36:43 +0100

    modelldefinition-mit-mongoose

--- backend/src/models/Link.ts
+++ backend/src/models/Link.ts
@@ -3,3 +3,26 @@ import mongoose, { Document, Schema } from "mongoose";
 const slug = require("mongoose-slug-generator");
 
 mongoose.plugin(slug);
+
+export interface Link extends Document {
+  url: string;
+  path: string;
+  slug: string;
+  created: Date;
+  modified: Date;
+}
+
+const linkSchema: Schema = new Schema(
+  {
+    url: { type: String, required: true },
+    path: { type: String, required: true, unique: true },
+    slug: { type: String, slug: "path", unique: true },
+  },
+  {
+    timestamps: { createdAt: "created", updatedAt: "modified" },
+  }
+);
+
+const Links = mongoose.model<Link>("Link", linkSchema);
+
+export default Links;
