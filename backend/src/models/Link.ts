3c72734bd5ca69c1e00d9638be1050820e524f6c
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-11-20 18:07:32 +0100

    --

--- backend/src/models/Link.ts
+++ backend/src/models/Link.ts
@@ -3,9 +3,3 @@ import mongoose, { Document, Schema } from "mongoose";
 const slug = require("mongoose-slug-generator");
 
 mongoose.plugin(slug);
-
-export interface Link extends Document{
-    url:string;
-    path: string;
-    slug: 
-}
