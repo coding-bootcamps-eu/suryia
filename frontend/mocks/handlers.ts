bdf42977ba16386f7e7942414cd815665ce91de7
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2024-01-25 22:16:56 +0100

    --

--- frontend/mocks/handlers.ts
+++ frontend/mocks/handlers.ts
@@ -1,7 +1,8 @@
-import { rest, RequestHandler } from 'msw'
+/*import { rest, RequestHandler } from 'msw'
+import type { Context } from 'vm'
 
 export const handlers: RequestHandler[] = [
-  rest.post('http://localhost:8080/login', async (req, res, ctx) => {
+  rest.post('http://localhost:8080/login', async (req: Request, res:Response, ctx:Context) => {
     const { username, password } = await req.json()
     if (username === 'julia12345@test.com' && password === 'password1234') {
       return res(ctx.json({ token: 'mocked-tocken' }))
