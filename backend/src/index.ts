0771852b5a853264295cb074bd8d31516ee7b747
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2024-02-08 15:22:31 +0100

    merge konflikt lösen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,31 +5,12 @@ import linkRoutes from "./routes/linkRoutes";
 
 const app = new App([userRoutes, statusRoutes, linkRoutes]);
 
-app.get("/corstest", (req: Request, res: Response) => {
-  res.json({
-    status: "ok",
-  });
-});
-
-app.use(
-  session({
-    secret: PASSPORT_SECRET,
-    resave: false,
-    saveUninitialized: false,
+app
+  .initializeDB()
+  .then(() => {
+    app.listen();
   })
-);
-const passportMiddleware = initializePassport();
-app.use(passportMiddleware.initialize());
-
-passport.use(new LocalStrategy(UserModel.authenticate()));
-passport.serializeUser(UserModel.serializeUser());
-passport.deserializeUser(UserModel.deserializeUser());
-
-app.use("/", userRoutes);
-app.use("/", statusRoutes);
-app.use("/", linkRoutes);
-
-//Express-Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  .catch((error) => {
+    console.error("Failed to initialize the application:", error);
+    process.exit(1);
+  });
