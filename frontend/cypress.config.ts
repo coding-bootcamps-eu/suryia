d2ec82b7a8a1bf86097a0625028179b13cfab19d
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2024-02-27 16:37:15 +0100

    fix bug

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: process.env.CYPRESS_BASE_URL
+    baseUrl: 'http://localhost:4173'
+  },
+  component: {
+    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite'
+    }
   }
 })
